f24c40c96e8d3927f90e3f52550d8f5eb7a50d9a
IanD1013 <ian.tung1013@gmail.com>
2025-04-04 10:18:36 +1300

    Implement status codes in rest api

--- RealEstateApi/Controllers/CategoriesController.cs
+++ RealEstateApi/Controllers/CategoriesController.cs
@@ -13,43 +13,48 @@ namespace RealEstateApi.Controllers
 
         // GET: api/<CategoriesController>
         [HttpGet]
-        public IEnumerable<Category> Get()
+        public IActionResult Get()
         {
-            return _dbContext.Categories;
+            //return StatusCode(StatusCodes.Status200OK);
+            return Ok(_dbContext.Categories);
         }
 
         // GET api/<CategoriesController>/5
         [HttpGet("{id}")]
-        public Category Get(int id)
+        public IActionResult Get(int id)
         {
-            return _dbContext.Categories.FirstOrDefault(c => c.Id == id);
+            var category = _dbContext.Categories.FirstOrDefault(c => c.Id == id);
+            return Ok(category);
         }
 
         // POST api/<CategoriesController>
         [HttpPost]
-        public void Post([FromBody] Category category)
+        public IActionResult Post([FromBody] Category category)
         {
             _dbContext.Categories.Add(category);
             _dbContext.SaveChanges();
+            return StatusCode(StatusCodes.Status201Created);
         }
 
         // PUT api/<CategoriesController>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] Category categoryObj)
+        public IActionResult Put(int id, [FromBody] Category categoryObj)
         {
             var category = _dbContext.Categories.Find(id);
             category.Name = categoryObj.Name;
             category.ImageUrl = categoryObj.ImageUrl;
             _dbContext.SaveChanges();
+            return Ok("Record updated successfully");
         }
 
         // DELETE api/<CategoriesController>/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public IActionResult Delete(int id)
         {
             var category = _dbContext.Categories.Find(id);
             _dbContext.Categories.Remove(category);
             _dbContext.SaveChanges();
+            return Ok("Record deleted");
         }
     }
 }
