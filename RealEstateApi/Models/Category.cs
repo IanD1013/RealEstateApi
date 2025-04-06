22e6c9e9c6fc69de374a1bfd6577f178f3356a4b
IanD1013 <ian.tung1013@gmail.com>
2025-04-07 09:00:59 +1200

    Add models, dbset properties & migrations

--- RealEstateApi/Models/Category.cs
+++ RealEstateApi/Models/Category.cs
@@ -9,5 +9,6 @@ namespace RealEstateApi.Models
         public string Name { get; set; }
         [Required(ErrorMessage = "Image url can't be empty")]
         public string ImageUrl { get; set; }
+        public ICollection<Property> Properties { get; set; }
     }
 }
