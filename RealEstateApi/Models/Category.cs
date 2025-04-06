e6670c7034e61f8b9b5e8072a4ae28426722746a
IanD1013 <ian.tung1013@gmail.com>
2025-04-07 08:13:50 +1200

    Implement validation in rest api

--- RealEstateApi/Models/Category.cs
+++ RealEstateApi/Models/Category.cs
@@ -1,9 +1,13 @@
-﻿namespace RealEstateApi.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RealEstateApi.Models
 {
     public class Category
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Category name can't be empty")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Image url can't be empty")]
         public string ImageUrl { get; set; }
     }
 }
