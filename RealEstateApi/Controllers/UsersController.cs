ba34aa6ce91816298a70fc1b59eddb78605656f0
IanD1013 <ian.tung1013@gmail.com>
2025-04-09 22:48:43 +1200

    Register method

---
+++ RealEstateApi/Controllers/UsersController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RealEstateApi.Data;
+using RealEstateApi.Models;
+
+namespace RealEstateApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        ApiDbContext _dbContext = new ApiDbContext();
+
+        [HttpPost("[action]")]
+        public IActionResult Register([FromBody] User user)
+        {
+            var userExists = _dbContext.Users.FirstOrDefault(u => u.Email == user.Email);
+            if (userExists != null)
+            {
+                return BadRequest("User with same email already exists");
+            }
+            _dbContext.Users.Add(user);
+            _dbContext.SaveChanges();
+            return StatusCode(StatusCodes.Status201Created);
+        }
+    }
+}
