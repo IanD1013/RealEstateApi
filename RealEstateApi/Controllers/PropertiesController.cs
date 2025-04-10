e169010642e9c48956aaef1c144a6ed4d6370239
IanD1013 <ian.tung1013@gmail.com>
2025-04-11 11:34:42 +1200

    Update property

--- RealEstateApi/Controllers/PropertiesController.cs
+++ RealEstateApi/Controllers/PropertiesController.cs
@@ -29,5 +29,29 @@ namespace RealEstateApi.Controllers
             _dbContext.SaveChanges();
             return StatusCode(StatusCodes.Status201Created);
         }
+
+        [HttpPut("{id}")]
+        [Authorize]
+        public IActionResult Put(int id, [FromBody] Property property)
+        {
+            var propertyResult = _dbContext.Properties.FirstOrDefault(p => p.Id == id);
+            if (propertyResult == null) return NotFound();
+
+            var userEmail = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Email)?.Value;
+            var user = _dbContext.Users.FirstOrDefault(u => u.Email == userEmail);
+            if (user == null) return NotFound();
+            if (propertyResult.UserId == user.Id)
+            {
+                propertyResult.Name = property.Name;
+                propertyResult.Detail = property.Detail;
+                propertyResult.Price = property.Price;
+                propertyResult.Address = property.Address;
+                property.IsTrending = false;
+                property.UserId = user.Id;
+                _dbContext.SaveChanges();
+                return Ok("Record updated successfully");
+            }
+            return BadRequest();
+        }
     }
 }
